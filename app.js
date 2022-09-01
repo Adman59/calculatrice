f9d7a88daa3112cfad099611c253b9f30e8ba4ba
adrien magnies <adrien.magnies@hotmail.fr>
2022-09-01 08:57:33 +0200

    modification du javascript avec une erreur sur touche a corriger

--- app.js
+++ app.js
@@ -2,5 +2,32 @@
 
 const touche = [...document.querySelectorAll('.bouton')];
 const listeKeycode = touche.map(matouche => matouche.dataset.key);
+const ecran = document.querySelector('.ecran');
 
-document.addEventListener('keydown', (e) => console.log(e))
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
+})
+
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    
+})
+
+const calculer = (valeur) => {
+    if(listeKeycode.includes(valeur)) {
+        switch(valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const matouche = touche[indexKeycode];
+                ecran.textContent += matouche.innerHTML;
+        }
+    }
+}
